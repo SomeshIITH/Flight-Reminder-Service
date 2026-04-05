f8cdab9c7282db8fef2d11be0ae0ba6f753e4186
SomeshIITH <shivjaiswal51230@gmail.com>
2026-04-05 21:30:55 +0530

    basic template

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {PORT} = require('./config/serverConfig.js');
+const apiRoutes = require('./routes/index.js');
+const db = require('./models/index.js');
+
+const SetupServer = async () => {
+    const app = express();
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use('/api' , apiRoutes);
+
+    app.listen(PORT,async () => {
+        console.log(PORT);
+        console.log(`Server is running on http://localhost:${PORT}`);
+        // if(process.env.SYNC_DB){
+        //     db.sequelize.sync({alter : true});
+        // }
+        
+    })
+}
+
+SetupServer();
---
+++ src/routes/v1/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+//add routing logic
+
+
+module.exports  = router
